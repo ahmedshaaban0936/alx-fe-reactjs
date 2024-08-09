b2af1d224a6ed2b0d95800a5159925276e9e2846
Ahmed AbdelHamid <dershaaban@gmail.com>
2024-08-09 22:51:31 +0300

    Last Commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';  // Import statement for WelcomeMessage
-import UserProfile from './components/UserProfile';
+import UserProfile from './components/UserProfile.jsx';
 function App() {
     return (
         <div> className="App"
