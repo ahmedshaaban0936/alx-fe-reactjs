c6656e650f4fe7acfd0953aa8e7131cd0bba8859
Ahmed AbdelHamid <dershaaban@gmail.com>
2024-08-18 16:46:04 +0300

    Third Commit

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -4,4 +4,19 @@ import React, { createContext } from 'react';
 const UserContext = createContext();
 
 export const UserProvider = UserContext.Provider;
+// src/UserDetails.jsx
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
 export default UserContext;
