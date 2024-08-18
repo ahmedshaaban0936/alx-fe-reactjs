d49ee4bb60ae2ff8bd16901525536aec3b265aaf
Ahmed AbdelHamid <dershaaban@gmail.com>
2024-08-18 17:39:37 +0300

    Third Commit

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+// src/components/UserProfile.jsx
+import React, { useContext } from 'react';
+import UserContext from '../UserContext'; // Import UserContext
 
-const UserProfile = (props) => {
-    return (
-        <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
-        </div>
-    );
-};
+function UserProfile() {
+  const userData = useContext(UserContext); // Use useContext to access context data
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
 
 export default UserProfile;
