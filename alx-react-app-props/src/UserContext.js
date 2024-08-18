2f8d1891da3bc7028a9029cd494000861527cdd8
Ahmed AbdelHamid <dershaaban@gmail.com>
2024-08-18 17:09:18 +0300

    Third Commit

--- alx-react-app-props/src/UserContext.js
+++
@@ -1,9 +0,0 @@
-// src/UserContext.js
-import React, { createContext } from 'react';
-
-const UserContext = createContext();
-
-export const UserProvider = UserContext.Provider;
-export default UserContext;
-
-
