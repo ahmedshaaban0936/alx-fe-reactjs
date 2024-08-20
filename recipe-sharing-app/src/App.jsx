bd2f8c0593955c6255514097a71a41db1985be08
Ahmed AbdelHamid <dershaaban@gmail.com>
2024-08-20 22:28:07 +0300

    4. Commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -9,6 +9,30 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
 
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SearchBar from './components/SearchBar';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+        </Routes>
+        <FavoritesList />
+        <RecommendationsList />
+      </div>
+    </Router>
+  );
+}
+
 function App() {
   return (
     <Router>
