06319ba64125eaed505408e61b3016ab00b79de4
Ahmed AbdelHamid <dershaaban@gmail.com>
2024-08-20 21:47:01 +0300

    2. Commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -5,14 +5,23 @@ import './App.css'
 import React from 'react';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeDetails from './components/RecipeDetails';
+
 function App() {
   return (
-    <div>
-      <h1>Recipe Sharing Application</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing Application</h1>
+        <AddRecipeForm />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
+
   return (
     <>
       <div>
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -6,5 +6,13 @@ export const useRecipeStore = create((set) => ({
   addRecipe: (newRecipe) => set((state) => ({
     recipes: [...state.recipes, newRecipe],
   })),
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== id),
+  })),
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+})),
   setRecipes: (recipes) => set({ recipes }),
 }));
