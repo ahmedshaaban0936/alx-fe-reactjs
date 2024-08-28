6b16a6746b738a2a9728aa4c5dce5d2fd60ffaec
Ahmed AbdelHamid <dershaaban@gmail.com>
2024-08-29 01:15:36 +0300

    4. Commit

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -2,11 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import TodoList from './components/TodoList'; // Import the TodoList component
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <div className="App">
+    <h1>Todo List</h1>
+    <TodoList /> {/* Use the TodoList component */}
+  </div>
+);
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -29,7 +36,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  
 }
 
 export default App
