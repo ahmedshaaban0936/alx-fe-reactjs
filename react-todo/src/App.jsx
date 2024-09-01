507e452c6df4ccbbb7c17fd9c2c207d913ed20e8
Ahmed AbdelHamid <dershaaban@gmail.com>
2024-09-01 19:48:51 +0300

    Implement and test TodoList component

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,3 +1,5 @@
+import logo from './logo.svg';
+import './App.css';
 import React from 'react';
 import TodoList from './components/ TodoList';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -10,6 +12,26 @@ function App() {
         {/* Add other routes here */}
       </Routes>
     </Router>
+  );
+}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
